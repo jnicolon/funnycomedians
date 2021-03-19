84b4f46d132db1a35aceed3110e06acd11ac6a04
jnicolon <63628726+jnicolon@users.noreply.github.com>
2021-03-19 01:04:51 -0400

    list css, hover and submit functionality

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 const searchBar = document.getElementById('search-bar');
 const searchSuggestions = document.querySelector('#search-suggestions');
+const searchContainer = document.querySelector('.search-container');
+
 
 searchBar.addEventListener('keyup', async (e)=>{
-
     try {
         if (e.target.value.length > 0){
+
+            searchSuggestions.innerHTML = "";
         
             const response = await fetch(`http://localhost:5000/api/${e.target.value}`);
     
@@ -15,23 +18,44 @@ searchBar.addEventListener('keyup', async (e)=>{
             searchResults.forEach(result =>{
                 searchSuggestions.innerHTML += `
                     <li class='search-suggestion'>
-                        <a href="/${result.comedian_name}">
-                            ${result.comedian_name}
+                        <a class="search-link" href="/${result.comedian_name}">
+                            <p class="hovered-link">${result.comedian_name}</p>
+                            
                         </a>
                     </li>`
 
-            })
+            });
+
+            const allLinks = document.querySelectorAll('.hovered-link');
+
             
 
+            allLinks.forEach(link =>{
+                link.addEventListener('mouseenter', ()=>{
+                    link.classList.add("selected");
 
+                })
 
-    
+                link.addEventListener('mouseleave', ()=>{
+                    link.classList.remove("selected");
+
+                })
+            })
+
+        } else {
+            searchSuggestions.innerHTML = "";
         }
 
     } catch (error) {
         console.log(error)
     }
-    
-
 });
 
+
+
+
+searchContainer.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    window.location = `/${searchBar.value}`
+    console.log(searchBar.value)
+}) 
