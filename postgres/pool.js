337579a90e393ec4079206e9db1b277a67c5f7d9
jnicolon <63628726+jnicolon@users.noreply.github.com>
2021-03-21 23:36:04 -0400

    pg connection fix

--- postgres/pool.js
+++ postgres/pool.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const Pool = require("pg").Pool;
 
-
 const pool = new Pool({
   user: process.env.SQL_USERNAME,
   host: process.env.SQL_HOST,
   database: process.env.SQL_DATABASE,
   password: process.env.SQL_PASSWORD,
   port: process.env.SQL_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = pool
--- server.js
+++ server.js
@@ -4,16 +4,8 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const pool = require('./postgres/pool');
+const pool = require('./postgres/pool');
 
-const {Client} = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
 app.use(cors());
 app.use(express.json());
@@ -27,40 +19,52 @@ app.get('/', async (req, res)=>{
 })
 
 app.get('/api/:comedians', async(req, res)=>{
+  try {
 
-  await client.connect();
   
-  const query = `SELECT 
-                        comedian_name, 
-                        id 
-                 FROM 
-                        comedians 
-                 WHERE 
-                        comedian_name ILIKE $1
-                 LIMIT 10`
+    const query = `SELECT 
+                          comedian_name, 
+                          id 
+                   FROM 
+                          comedians 
+                   WHERE 
+                          comedian_name ILIKE $1
+                   LIMIT 10`
+  
+  pool.query(query, [`${req.params.comedians}%`], (err, psRes)=>{
+      err ? console.log(err) : res.json(psRes.rows)  })
+  
 
-client.query(query, [`${req.params.comedians}%`], (err, psRes)=>{
-    err ? console.log(err) : res.json(psRes.rows)  })
 
-    await client.end()
+  } catch (error) {
+    console.log(error)
+  }
+
+
 })
 
 app.get('/:comedianName', async (req, res)=>{
 
-  await client.connect();
+  try {
+    
 
-  const query = `SELECT 
-                        *
-                        FROM 
-                          comedians 
-                        WHERE 
-                          comedian_name = $1
-                        `
+    const query = `SELECT 
+                          *
+                          FROM 
+                            comedians 
+                          WHERE 
+                            comedian_name = $1
+                          `
+  
+  pool.query(query, [`${req.params.comedianName}`], (err, psRes)=>{
+  err ? console.log(err) :  res.render('./pages/comedian', {data: psRes.rows[0]})  })
+  
+  
+  } catch (error){
+    console.log(error)
+  }
 
-client.query(query, [`${req.params.comedianName}`], (err, psRes)=>{
-err ? console.log(err) :  res.render('./pages/comedian', {data: psRes.rows[0]})  })
-
-await client.end();
+  
  
 })
 
